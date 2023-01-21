7693c9635ccb9ab01470a41d90eec6c2a304870c
suhanasalma <108382046+suhanasalma@users.noreply.github.com>
2023-01-21 20:47:43 +0600

    movie page loaded

--- src/Pages/Movies/Movie/movieStyles.js
+++ src/Pages/Movies/Movie/movieStyles.js
@@ -1,18 +1,40 @@
 import { makeStyles } from "@mui/styles";
 import { padding } from "@mui/system";
 
-export default makeStyles(theme=>({
-   movie:{
-      padding:"10px"
-   },
-   title:{
-      color:theme.palette.text.primary,
-      textOverflow:'ellipsis',
-      width:'230px',
-      whiteSpace:'nowrap',
-      overflow:'hidden',
-      marginTop:'10px',
-      marginBottom:0,
-      textAlign:'center'
-   }
-}))
+export default makeStyles((theme) => ({
+  movie: {
+    padding: "10px",
+  },
+  link: {
+    alignItems: "center",
+    textDecoration: "none",
+    fontWeight: "bolder",
+    [theme.breakpoints.up("xs")]: {
+      display: "flex",
+      flexDirection: "column",
+    },
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
+
+  image: {
+    borderRadius: "20px",
+    height: "300px",
+    marginBottom: "10px",
+    transition:'ease-in-out .2s',
+    "&:hover": {
+      transform: "scale(1.05)",
+    },
+  },
+  title: {
+    color: theme.palette.text.primary,
+    textOverflow: "ellipsis",
+    width: "230px",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    marginTop: "10px",
+    marginBottom: 0,
+    textAlign: "center",
+  },
+}));
